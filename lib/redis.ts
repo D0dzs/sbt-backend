b3669918728b457e06beafd6dc1fc9029cda8b1a
Dodzs <gameeaters9@gmail.com>
2025-03-14 18:13:34 +0100

    Update redis.ts

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,7 @@
 import Redis from "ioredis";
 
-const redisClient = new Redis(process.env.REDIS_URL!, {
+// https://docs.railway.com/reference/errors/enotfound-redis-railway-internal
+const redisClient = new Redis(`${process.env.REDIS_URL!}?family=0`, {
   connectTimeout: 10000,
   retryStrategy: (times) => Math.min(times * 50, 2000),
 });
