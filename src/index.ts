9dc72f41c86c0ffc096cfa6af9743db1e5fc664e
Dodzs <51715950+D0dzs@users.noreply.github.com>
2025-03-18 08:18:19 +0100

    Update index.ts 3

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ import imageRouter from "./routers/imageProvider.router";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
